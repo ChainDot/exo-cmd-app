87d39a81cdb4cea178b6fdbdb928bdf242ddebfd
ChainDot <crypto.dot@protonmail.com>
2021-03-19 09:30:47 +0100

    update

--- pyramid.js
+++ pyramid.js
@@ -1,7 +1,6 @@
-const pyramid = (nb, str,reverse) => {
-  str = process.argv[3]
-  reverse = process.argv[4]
-  if(process.argv.length === 5 && process.arg[4] === '-r'){
+const pyramid = (nb, str,r) => {
+  
+  if(process.argv.length === 5 && r === '-r'){
   for (let i = nb; i >= 1; --i) {
     console.log(str.repeat(i))
   }
@@ -10,19 +9,27 @@ const pyramid = (nb, str,reverse) => {
       console.log(str.repeat(i))
       }
     }
-  }
-if(process.argv.length !== 4){
-  console.log('usage: node Pyramid.js number string')
+}
+
+
+if(process.argv.length === 5 && process.argv[4] !== '-r' ){
+  console.log('usage: node Pyramid.js number string -r')
   process.exit(1)
 }
 
-if (isNaN(process.argv[2])) {
+if(isNaN(process.argv[2])){
   console.log(`Error: ${process.argv[2]} is not a number.`)
   process.exit(1)
 }
 
+if(process.argv.length < 4){
+  console.log('usage: node Pyramid.js number string')
+  process.exit(1)
+}
 
 let res = Number(process.argv[2])
+let str = process.argv[3]
+let r = process.argv[4]
+pyramid(res, str,r)
 
-pyramid(res)
 
